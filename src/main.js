5ae28cca5d69fd8263093002a4d06ea84886de82
15114565130 <1422425666@qq.com>
2017-07-17 09:09:05 +0800

    首页

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
 
+Vue.use(ElementUI, { locale })
+
+
+import '@/assets/app'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-
-  
   template: '<App/>',
   components: { App }
 })
