5097986d97b1d551802b5ec56ad09cc6e5554c6a
YRz <2451916046@qq.com>
2017-07-19 14:54:49 +0800

    段子样式

--- src/views/duanzi/duanzi.vue
+++ src/views/duanzi/duanzi.vue
@@ -1,7 +1,11 @@
 <template>
   <div >
        <div v-for="item in num" class="duanzi">
-          <span>{{item.text}}</span>
+          <span class="head">
+            <img class="img" v-bind:src="item.user.avatar_url">
+            <a href="#" class="user_name">{{item.user.name}}</a>
+          </span></br>
+          <a class="text">{{item.text}}</a>
        </div>
   </div>
 </template>
@@ -23,11 +27,12 @@ export default {
       var that = this;
       var url = "http://localhost/src/assets/php/post1.php?id=-102";
       Axios.get(url).then(response => {
-          console.log(response.data.data.data);
+          // console.log(response.data.data.data);
           this.data =response.data.data.data;
            for (var i = 0; i < that.data.length; i++) {
               this.num.push(that.data[i].group);
             }
+            console.log(this.num)
       }, response => {
           // error callback
       })
@@ -36,5 +41,36 @@ export default {
 }
 </script>
 <style>
-  .duanzi{width:80%;height:auto;border: 1px solid red; padding: 20px;}
+  .duanzi{
+    width:80%;
+    height:auto;
+    border: 1px solid #ccc; 
+    padding: 20px;
+    margin: 5px auto;
+    background: #ccc;
+  }
+  .head{
+    width: 100%;
+    float: left;
+  }
+  .img{
+    border-radius: 40px;
+    width: 40px;
+    height: 40px;
+    float: left;
+  }
+  .user_name{
+    display: block;
+    float: left;
+    line-height: 40px;
+    text-align: center;
+    font-size: 14px;
+    color: black;
+    margin-left: 7px;
+  }
+  .text{
+    display: block;
+    font-size: 16px;
+    line-height: 20px;
+  }
 </style>
